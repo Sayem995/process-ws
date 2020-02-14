59db32f52a292064c540b19993b21aa976c38cbc
Sayem995 <sayem.bokhari.1@gmail.com>
2020-02-14 21:35:41 +0000

    wrote second failing test

--- spec/frequencies_spec.rb
+++ spec/frequencies_spec.rb
@@ -5,5 +5,10 @@ describe Frequencies do
     it "should return the given soundwaves if within range of frequencies" do
       expect(subject.soundwave([60],40,1000)).to eq [60]
     end
+
+    it "should return lower limit if input is under lower limit" do
+      expect(subject.soundwave([20],40,1000)).to eq [40]
+    end
+
   end
 end
