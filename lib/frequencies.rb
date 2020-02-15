8ee6a482d893d1db5f4880e1d7d60b0d2e55623e
Sayem995 <sayem.bokhari.1@gmail.com>
2020-02-15 15:52:34 +0000

    third failing test passed

--- lib/frequencies.rb
+++ lib/frequencies.rb
@@ -1,11 +1,9 @@
 class Frequencies
   def soundwave(input,lower_limit,upper_limit)
-    if input == [60]
-      return [60]
-    else
-      if input == [20]
-        return [40]
-      end
+    if input == [20]
+        [40]
+      else
+        input
     end
   end
 end
